540ec991886c9ec51f4990c37e6bba044a940228
Sumit saha <smtsh9@gmail.com>
2020-10-28 11:04:38 +0530

    cicd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "./.env") });
 const express = require("express");
+const path = require("path");
+//require("dotenv").config({ path: path.join(__dirname, "./.env") });
+// const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
